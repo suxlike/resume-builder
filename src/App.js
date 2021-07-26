f1987b6d01139a5714f20803d4a544d5764351ab
Umut Toker <utoker@gmail.com>
2021-07-26 19:28:06 -0400

    add work and education input forms

--- src/App.js
+++ src/App.js
@@ -24,22 +24,23 @@ function App() {
     { github: "asd@github" },
     { linkedin: "as" },
   ]);
-  const [company, setCompany] = useState("");
-  const [position, setPosition] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [description, setDescription] = useState("");
-  const [course, setCourse] = useState("");
-  const [uni, setUni] = useState("");
-  const [uniStartDate, setUniStartDate] = useState("");
-  const [uniEndDate, setUniEndDate] = useState("");
-  const [uniDescription, setUniDescription] = useState("");
+  const [edu, setEdu] = useState([
+    {
+      course: "asd",
+      university: "asd",
+      startDate: "2015",
+      endDate: "2016",
+      description: "description",
+    },
+  ]);
   const [work, setWork] = useState([
-    { company: "asd" },
-    { position: "asd" },
-    { startDate: "2015" },
-    { endDate: "2016" },
-    { description: "description" },
+    {
+      company: "asd",
+      position: "asd",
+      startDate: "2015",
+      endDate: "2016",
+      description: "description",
+    },
   ]);
 
   const classes = useStyles();
@@ -57,23 +58,10 @@ function App() {
               Fill out the form{" "}
             </Typography>
             <SetInfo setInfos={setInfos} infos={infos} />
-            <SetWork
-              work={work}
-              setWork={setWork}
-              setCompany={setCompany}
-              setPosition={setPosition}
-              setStartDate={setStartDate}
-              setEndDate={setEndDate}
-              setDescription={setDescription}
-            />
             <Divider variant="middle" />
-            <SetEdu
-              setCourse={setCourse}
-              setUni={setUni}
-              setUniStartDate={setUniStartDate}
-              setUniEndDate={setUniEndDate}
-              setUniDescription={setUniDescription}
-            />
+            <SetWork work={work} setWork={setWork} />
+            <Divider variant="middle" />
+            <SetEdu edu={edu} setEdu={setEdu} />
           </Card>
         </Grid>
         <Grid item md={6}>
